ed2682c3a7b13b20507bfed132c141a7b83dfb5f
Alyona <borisovsk_a@mail.ru>
2023-02-17 12:56:57 +0300

    feat: Start / stop TimerService

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -1,15 +1,64 @@
 package com.example.services
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
 
+    private lateinit var timerService: TimerService
+
+    var isBound = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        mainBinding.startBtn.setOnClickListener {
+            startTimer()
+        }
+        mainBinding.stopBtn.setOnClickListener {
+            stopTimer()
+        }
+    }
+
+    private inner class TimerServiceConnection: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as TimerService.LocalTimeServiceBinder
+            timerService = binder.getTimerService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+            Log.d(TAG, "onServiceDisconnected() called with: name = $name")
+        }
+    }
+
+    private fun startTimer() {
+        intent = Intent(this, TimerService::class.java)
+        startService(intent)
+    }
+
+    private fun stopTimer() {
+        intent = Intent(this, TimerService::class.java)
+        stopService(intent)
+    }
+
+    private fun bindService() {
+        intent = Intent(this, TimerService::class.java)
+//        val timerServiceConnection = ServiceConnection
+//        bindService(intent, timerServiceConnection, BIND_AUTO_CREATE)
+    }
+
+    companion object {
+        const val TAG = "MainActLog"
     }
 }
--- app/src/main/java/com/example/services/TimerService.kt
+++ app/src/main/java/com/example/services/TimerService.kt
@@ -2,15 +2,92 @@ package com.example.services
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
+import android.widget.TimePicker.OnTimeChangedListener
 
 class TimerService : Service() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+
+    lateinit var onTimeChangedListener: OnTimeChangedListener
+
+    private val myBinder = LocalTimeServiceBinder()
+    private lateinit var countdownTimer: CountDownTimer
+
+    override fun onBind(intent: Intent): IBinder? {
+        Log.d(TAG, "onBind() called with: intent = $intent")
+        return myBinder
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate() called")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        Log.d(TAG,
+            "onStartCommand() called with: intent = $intent, flags = $flags, startId = $startId")
+
+        startCountDownTimer(TIME_COUNTDOWN, TIMER_PERIOD)
+
+//        if(intent.action == ACTION_CLOSE) {
+//            // stopSelf()
+//        } else {
+//             startCountDownTimer(TIME_COUNTDOWN, TIMER_PERIOD)
+//        }
+
+        return START_NOT_STICKY
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+        Log.d(TAG, "onUnbind() called with: intent = $intent")
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind() called with: intent = $intent")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+        stopCountdownTimer()
+    }
+
+    inner class LocalTimeServiceBinder : Binder() {
+        fun getTimerService() = this@TimerService
+    }
+
+    private fun startCountDownTimer(time: Long, period: Long) {
+        countdownTimer = object: CountDownTimer(time, period) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d(TAG, "onTick() called with: millisUntilFinished = $millisUntilFinished")
+            }
+
+            override fun onFinish() {
+                Log.d(TAG, "onFinish() called")
+                stopCountdownTimer()
+            }
+        }
+
+        countdownTimer.start()
+    }
+
+    private fun stopCountdownTimer() {
+        Log.d(TAG, "stopCountdownTimer() called")
+        countdownTimer.cancel()
+        // remove link
+    }
+
+    companion object {
+        const val TAG = "TimerServTag"
+        const val ACTION_CLOSE = "TIMER_ACTION_CLOSE"
+        // TODO откуда берем?
+        const val TIME_COUNTDOWN = 1000 * 500L
+        const val TIMER_PERIOD = 1000L
     }
 }
