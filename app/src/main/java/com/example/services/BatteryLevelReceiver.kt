d1183e66a9dc2c78277c8d13ad5424490e49e177
Alyona <borisovsk_a@mail.ru>
2023-02-20 13:04:44 +0300

    refactor: Battery manager.EXTRA_LEVEL

--- app/src/main/java/com/example/services/BatteryLevelReceiver.kt
+++ app/src/main/java/com/example/services/BatteryLevelReceiver.kt
@@ -4,8 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class BatteryLevelReceiver: BroadcastReceiver {
+class BatteryLevelReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         // TODO create and show notification
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            context.registerReceiver(null, ifilter)
+        }
+        val batteryPct: Float? = batteryStatus?.let { intent ->
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            level * 100 / scale.toFloat()
+        }
+
     }
 }
